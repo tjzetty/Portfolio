a548cbfd9175cd54a3a2448684a823717b9d049d
tjzetty <tjzetty@mtu.edu>
2022-03-28 16:55:08 -0400

    adding dist subtree

---
+++ dist/assets/AboutView.187f69da.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.6623d712.js";import{o as t,b as o,f as _}from"./vendor.4f9f00da.js";const s={},a={class:"about"},c=_("h1",null,"This is an about page",-1),n=[c];function r(i,u){return t(),o("div",a,n)}var f=e(s,[["render",r]]);export{f as default};
